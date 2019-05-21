ecf87d6c567e60484621515c22a800c05789ac48
dees040 <deesoomens@gmail.com>
2019-05-21 13:58:48 +0000

    Uncomment all commented lines of commands in the Envoy file

--- src/Envoy.blade.php
+++ src/Envoy.blade.php
@@ -1,14 +1,14 @@
 @servers(['localhost' => '127.0.0.1'])
 
 @task('documentation')
-    # composer install --no-interaction --prefer-dist --optimize-autoloader --quiet
+    composer install --no-interaction --prefer-dist --optimize-autoloader --quiet
 
-    # vendor/bin/phpunit --no-coverage > /dev/null 2>&1
+    vendor/bin/phpunit --no-coverage > /dev/null 2>&1
 
     php artisan api:docs
 
     @if (! $keepDev)
-        # composer install --no-interaction --prefer-dist --optimize-autoloader --quiet --no-dev
+        composer install --no-interaction --prefer-dist --optimize-autoloader --quiet --no-dev
     @endif
 
     aglio -i public/docs/output.apib --theme-template triple -o public/docs/output.html
